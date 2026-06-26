f72569e51ed2b0d9124e493a91035bbe93dd7ab6
Shuo Wu <sw6529@nyu.edu>
2026-06-26 01:10:38 -0400

    Complete 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Display = ({text, state}) => {
+  return (
+    <p>{text} {state}</p>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header text={"give feedback"}/>
+      <Button onClick={() => setGood(good + 1)} text={"good"}/>
+      <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"}/>
+      <Button onClick={() => setBad(bad + 1)} text={"bad"}/>
+      <Header text={"statistics"}/>
+      <Display text={"good"} state={good}/>
+      <Display text={"neutral"} state={neutral}/>
+      <Display text={"bad"} state={bad}/>
+    </div>
+  )
+}
+
+export default App
