569d557fbded911acaff960ee385556bfddcd0b5
Shuo Wu <sw6529@nyu.edu>
2026-06-26 01:24:08 -0400

    complete 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -23,17 +23,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [point, setPoint] = useState(0)
+
+  const handleGood = () => {
+    const newGood = good + 1
+    setGood(newGood)
+    setTotal(newGood + neutral + bad)
+    setPoint(point + 1)
+  }
+
+  const handleNeutral = () => {
+    const newNeutral = neutral + 1
+    setNeutral(newNeutral)
+    setTotal(good + newNeutral + bad)
+    setPoint(point + 0)
+  }
+  
+  const handleBad = () => {
+    const newBad = bad + 1
+    setBad(newBad)
+    setTotal(good + neutral + newBad)
+    setPoint(point - 1)
+  }
 
   return (
     <div>
       <Header text={"give feedback"}/>
-      <Button onClick={() => setGood(good + 1)} text={"good"}/>
-      <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"}/>
-      <Button onClick={() => setBad(bad + 1)} text={"bad"}/>
+      <Button onClick={handleGood} text={"good"}/>
+      <Button onClick={handleNeutral} text={"neutral"}/>
+      <Button onClick={handleBad} text={"bad"}/>
       <Header text={"statistics"}/>
       <Display text={"good"} state={good}/>
       <Display text={"neutral"} state={neutral}/>
       <Display text={"bad"} state={bad}/>
+      <Display text={"all"} state={total}/>
+      <Display text={"average"} state={point / total}/>
+      <Display text={"positive"} state={good / total}/>
     </div>
   )
 }
