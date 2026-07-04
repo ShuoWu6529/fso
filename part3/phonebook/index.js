1ccbacd2ff5ee5bc7c781782fc6eef0ceb0d8f24
Shuo Wu <sw6529@nyu.edu>
2026-07-04 01:32:21 -0400

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 const data = (request, response) => {
     if (request.method === "POST") {
