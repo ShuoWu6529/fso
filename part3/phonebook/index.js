af8ff778c963b1b05043b9d86854cf3b05910ea3
Shuo Wu <sw6529@nyu.edu>
2026-07-05 01:33:21 -0400

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -60,22 +60,20 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(person);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -108,6 +106,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({error : error.message})
   }
   next(error);
 };
