20cab2ec37d736f48d599cb033271f2bbec6ce7a
Shuo Wu <sw6529@nyu.edu>
2026-07-05 01:44:08 -0400

    uibuild

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 10,
+        validate: {
+            validator: (v) => {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
